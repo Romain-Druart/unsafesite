5e9cf9cd6f97863a2a0a052b3a4b8ec4b7e0f35c
Romain-Druart <druart.romainpro@gmail.com>
2021-07-26 10:33:05 +0200

    Changes to app.js : adding https | Add picture when logged

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
+const https = require("https");
+fs = require("fs");
 
+const options = {
+  // key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
+  // cert: fs.readFileSync("/srv/www/keys/chain.pem")
+};
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }), (req, res) => {
+  res.writeHead(200);
+  res.end("hello world\n");
+});
 const mysql = require('mysql');
+const { stringify } = require("querystring");
 
 const pool = mysql.createPool({
     connectionLimit : 100, //important
     host     : 'localhost',
     user     : 'root',
-    password : 'root',
-    database : 'monsite',
+    password : '',
+    database : 'unsafesite_db',
     debug    :  false
 });
 
@@ -33,25 +43,25 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  let request= "SELECT * FROM site_user where name='"+req.body.uname+"' and password='"+req.body.psw+"';"
+  let request= "SELECT * FROM site_user where name=? and password= ? "
   console.log(request)
-  pool.query(request,(err, data) => {
+  pool.query(request, [(req.body.uname), (req.body.psw)],(err, data) => {
     if(err) {
       console.error(err);
       res.render("login", {errorMessage:"Erreur interne, reassayer ultérieurement..."});
       return;
     }
     if( data.length > 0){
-      res.render("login", {errorMessage:"Connexion ok"});
+      res.render("loged", {errorMessage:"Connexion ok"});
     }else{
       res.render("login", {errorMessage:"nom d'utilisateur ou mot de pass incorrect"});
     }
   });
 });
 
-
-
 app.use("/", router);
 app.listen(process.env.port || 3000);
 
-console.log("Running at Port 3000");
+https.createServer(options, app).listen(3333)
+
+console.log("Running at Port 3000. Https runing at Port 3333");
